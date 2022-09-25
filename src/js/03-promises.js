4d6784f4ac4936363fccc7a8967b8137e49218b4
AlexHerbin <101976159+AlexHerbin@users.noreply.github.com>
2022-09-25 13:01:49 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,53 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  formEl: document.querySelector('.form'),
+  delayEl: document.querySelector('input[name="delay"]'),
+  stepEl: document.querySelector('input[name="step"]'),
+  amountEl: document.querySelector('input[name="amount"]'),
+};
+
+refs.formEl.addEventListener('submit', onBtnClick);
+
+function onBtnClick(e) {
+  e.preventDefault();
+
+  let { delay, step, amount } = onGettingData();
+
+  for (let position = 1; position <= amount; position+=1) {
+    createPromise(position, delay)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+    delay += step;
+  }
+};
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => { 
+
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(()=>{
+      if(shouldResolve) {
+        resolve({position, delay});
+      } else {
+        reject({position, delay});
+      }
+    },delay)
+  })
+};
+
+function onGettingData() {
+  return {
+    delay: Number(refs.delayEl.value),
+    step: Number(refs.stepEl.value),
+    amount: Number(refs.amountEl.value),
   }
 }
+
+
